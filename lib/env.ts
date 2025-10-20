8d25e294dc2ac07fc3a6ae61711579278ed09c50
pablotm-dev <pablo.toldo3@outlook.com>
2025-10-20 04:19:38 -0300

    Teste commit

--- lib/env.ts
+++ lib/env.ts
@@ -1,2 +1,2 @@
-//export const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || "https://179.190.40.40:8081/api"
-export const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:8081/api"
+export const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || "https://calendar-management-bu7d.onrender.com/api"
+//export const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:8081/api"
